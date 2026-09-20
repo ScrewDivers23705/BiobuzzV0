5af00454af654bddfea5c60d447ea7c5fb498a4e
KBjona <Jona10112010@gmail.com>
2026-09-20 11:49:33 +0300

    Pulled updated sdk and quickstart

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Tuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Tuning.java
@@ -1,18 +1,5 @@
 package org.firstinspires.ftc.teamcode.pedro;
 
-import com.pedropathing.revhub.drivetrains.Mecanum;
-import com.pedropathing.revhub.localizers.TwoWheelLocalizer;
-import com.pedropathing.tuning.autotune.Procedure;
-import org.firstinspires.ftc.teamcode.pedro.procedures.ForesightTuner;
-import org.firstinspires.ftc.teamcode.pedro.procedures.MecanumTuner;
-import org.firstinspires.ftc.teamcode.pedro.procedures.TwoWheelTuner;
-
-public class Tuning { // go to http://192.168.43.1.10158
-    public static Procedure mecanumTuner() {
-        return new MecanumTuner();
-    }
-    public static Procedure twoWheelTuner() {
-        return new TwoWheelTuner();
-    }
-
+public class Tuning {
+    // Tuners go here
 }
